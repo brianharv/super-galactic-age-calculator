5e391563d1f4029c31234f691375853bb3f1ebc9
Brian Harvey <brian.harv3y@gmail.com>
2020-09-04 10:39:02 -0700

    update spec 1 test to be more specific

--- _test_/planet-age.test.js
+++ _test_/planet-age.test.js
@@ -5,5 +5,6 @@ describe('User', () => {
   test('User class constructor creates User with age and gender properties', () => {
     const newUser = new User(32, "male");
     expect(newUser.age).toEqual(32);
+    expect(newUser.gender).toBe("male");
   });
 });
