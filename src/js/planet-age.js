2c0ef0ea9b55982d00c91fdc4b9024890216416f
Brian Harvey <brian.harv3y@gmail.com>
2020-09-04 11:13:26 -0700

    write method for ageMercury to continue tests

--- src/js/planet-age.js
+++ src/js/planet-age.js
@@ -2,8 +2,10 @@ export default class User {
   constructor(age, gender) {
     this.age = age;
     this.gender = gender
+    this.mercuryAge = 0;
   }
   ageMercury() {
-    
+  let ageConvert = this.age / .24;
+    return ageConvert += this.mercuryAge;
   }
 }
