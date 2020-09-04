765ed695cb34dbe7971f347f478169c7a91a42cf
Brian Harvey <brian.harv3y@gmail.com>
2020-09-04 10:35:11 -0700

    spec 1 fail test

--- _test_/planet-age.test.js
+++ _test_/planet-age.test.js
@@ -4,6 +4,6 @@ describe('User', () => {
 
   test('User class constructor creates User with age and gender properties', () => {
     let newUser = new User(age, gender);
-    expect(newUser(32, "male")).toBeInstanceOf(User);
+    expect(newUser).toBeInstanceOf(User);
   });
 });
