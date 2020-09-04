e5bd408e95cb2d8fcda9935d3625c059be45a55d
Brian Harvey <brian.harv3y@gmail.com>
2020-09-04 10:33:46 -0700

    spec one test fail

--- _test_/planet-age.test.js
+++ _test_/planet-age.test.js
@@ -1,10 +1,9 @@
 import User from '../src/js/planet-age.js';
-import { TestScheduler } from 'jest';
 
 describe('User', () => {
 
   test('User class constructor creates User with age and gender properties', () => {
     let newUser = new User(age, gender);
-    expect(newUser).toBeInstanceOf(User);
+    expect(newUser(32, "male")).toBeInstanceOf(User);
   });
 });
